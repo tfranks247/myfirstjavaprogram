d8fef81b0c62adb53c4b67d216b3e4fa39f089fc
tfranks247 <tfranks247@gmail.com>
2019-10-21 16:16:08 -0400

    new changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args){
+        //Printing out hello world to the console
         System.out.println("Hello World");
+        //declaration of  a variable
+        String name;
+        //initialization of a variable
+        name = "Tony";
+        //declaration and initialization of a variable
+        String newName = "Brad";
+        //reinitializing a variable
+        name = "Sam";
+        System.out.print(name);
+        System.out.println("Last Name");
     }
 }
